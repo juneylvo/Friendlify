51cf9a437ac0aef943c4062937503fdfc9399887
Jack Wang <jaack.wang@mail.utoronto.ca>
2024-12-02 22:44:27 -0500

    Defining all the tests for the friend profile use case

--- SpotifyUserLogin/src/Test/Java/TestUsers/TestFriendProfileUseCase.java
+++ SpotifyUserLogin/src/Test/Java/TestUsers/TestFriendProfileUseCase.java
@@ -1,4 +1,35 @@
+package TestUsers;
+
+import entities.users.FriendProfile;
 import junit.framework.TestCase;
+import useCase.FriendsList.FriendProfileUseCase;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class TestFriendProfileUseCase extends TestCase {
-  
+    private static final String username = "Username";
+    private static final String userId = "UserId";
+    private static final List<String> genres = Arrays.asList("Genre 1", "Genre 2", "Genre 3");
+    private static final List<String> artists = Arrays.asList("Artist 1", "Artist 2", "Artist 3");
+
+    private static final FriendProfile friendProfile = new FriendProfile(username, userId, genres, artists);
+
+    private static final FriendProfileUseCase friendProfileUseCase = new FriendProfileUseCase(friendProfile);
+
+    public void testGetPreferredGenres() {
+        assertEquals(friendProfileUseCase.getPreferredGenres(), Arrays.asList("Genre 1", "Genre 2", "Genre 3"));
+    }
+
+    public void testGetPreferredArtists() {
+        assertEquals(friendProfileUseCase.getPreferredArtists(), Arrays.asList("Artist 1", "Artist 2", "Artist 3"));
+    }
+
+    public void testGetUsername() {
+        assertEquals(friendProfileUseCase.getUsername(), username);
+    }
+
+    public void testFriendProfile() {
+        assertEquals(friendProfile, friendProfileUseCase.friendProfile());
+    }
 }

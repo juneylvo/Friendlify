944ad140c0861512d6e0b3afd5a2b5e96e1e7bf3
Jack Wang <jaack.wang@mail.utoronto.ca>
2024-12-02 22:44:38 -0500

    Initializing friends list use case test file

---
+++ SpotifyUserLogin/src/Test/Java/TestUsers/FriendsListUseCaseTest.java
@@ -0,0 +1,15 @@
+package TestUsers;
+
+import junit.framework.TestCase;
+
+public class FriendsListUseCaseTest extends TestCase {
+
+    public void testGetFriendsListNames() {
+    }
+
+    public void testGetUserProfile() {
+    }
+
+    public void testGetFriendProfileList() {
+    }
+}
